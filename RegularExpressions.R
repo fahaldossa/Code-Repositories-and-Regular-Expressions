318269431653284c2a22e7a07e9203ade8064c71
fahaldossa <bt21982@qmul.ac.uk>
2024-01-30 13:09:50 +0000

    they to the using ^ and $

--- RegularExpressions.R
+++ RegularExpressions.R
@@ -1,4 +1,4 @@
-#Regular Expressions: grep() function
+#2.1 Regular Expressions: grep() function
 #we want to find the word 'the' from the sentece
 sentence <- c('By','the','time','they', 'got', 'back,', 'the', 'lights', 'were', 'all', 'out', 'and', 'everybody', 'was', 'asleep.', 'Everybody,', 'that', 'is,', 'except', 'for', 'Guih', 'Kyom', 'the', 'dung', 'beetle.', 'He', 'was', 'wide', 'awake', 'and', 'on', 'duty,', 'lying', 'on', 'his', 'back', 'with', 'his', 'legs', 'in', 'the', 'air', 'to', 'save', 'the', 'world', 'in', 'case', 'the', 'heavens', 'fell.')
 grep_out <- grep(pattern = 'the', x = sentence)
@@ -10,3 +10,12 @@ sentence[grep_out]
 ##"the"  "they" "the"  "the"  "the"  "the"  "the" 
 
 #Note that this returned the word 'they'
+#To avoid this use anchors ^ and $ and change the pattern argument
+#^ is used to match the start and $ is used to match the end
+grep_out2 <- grep(pattern = '^the$', x = sentence)
+grep_out2
+sentence[grep_out2]
+##[1] "the" "the" "the" "the" "the" "the"
+
+#2.2 
+
